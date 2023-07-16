94d94c60db06764c64351eb2f7de6d32800910ee
chrimora <chris.d.moran@live.co.uk>
2023-07-16 18:24:32 +0100

    display history

--- src/app/play/history/page.tsx
+++ src/app/play/history/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-export default function Game() {
+import HistoryStore from "../history";
+
+export default function History() {
   function wipeStorage() {
     if (
       confirm(
@@ -15,9 +17,16 @@ export default function Game() {
 
   return (
     <>
-      TODO; Read history from local store
-      <br />
-      <br />
+      <div className="mx-auto text-sm sm:text-lg">
+        {HistoryStore.get().map((game) => (
+          <div className="flex border-b">
+            <div className="p-1 basis-0 grow text-right">{game[0].name}</div>
+            <div className="p-1 basis-8 text-right">{game[0].score}</div>
+            <div className="p-1 basis-8 text-left">{game[1].score}</div>
+            <div className="p-1 basis-0 grow text-left">{game[1].name}</div>
+          </div>
+        ))}
+      </div>
       <button
         className="p-4 m-8 border-2 border-accent-light dark:border-accent-dark active:bg-accent-light active:dark:bg-accent-dark hover:bg-accent-light hover:dark:bg-accent-dark"
         onClick={wipeStorage}
