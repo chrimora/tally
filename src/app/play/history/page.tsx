28d9b0f1d08a94e9dcc36ae691d8626950bf8a69
chris <chris@tail.cash>
2023-07-31 20:30:13 +0100

    reverse history - recent at top

--- src/app/play/history/page.tsx
+++ src/app/play/history/page.tsx
@@ -6,16 +6,20 @@ export default function Page() {
   return (
     <>
       <div className="mx-auto text-sm sm:text-lg">
-        {History.get().games.map((g) => (
-          <div key={`${g.id}`} className="flex border-b">
-            <div className="p-1 basis-0 grow text-right">
-              {g.scores[0].name}
+        {History.get()
+          .games.reverse()
+          .map((g) => (
+            <div key={`${g.id}`} className="flex border-b">
+              <div className="p-1 basis-0 grow text-right">
+                {g.scores[0].name}
+              </div>
+              <div className="p-1 basis-8 text-right">{g.scores[0].score}</div>
+              <div className="p-1 basis-8 text-left">{g.scores[1].score}</div>
+              <div className="p-1 basis-0 grow text-left">
+                {g.scores[1].name}
+              </div>
             </div>
-            <div className="p-1 basis-8 text-right">{g.scores[0].score}</div>
-            <div className="p-1 basis-8 text-left">{g.scores[1].score}</div>
-            <div className="p-1 basis-0 grow text-left">{g.scores[1].name}</div>
-          </div>
-        ))}
+          ))}
       </div>
       <button
         className="p-4 m-8 border-2 border-accent-light dark:border-accent-dark active:bg-accent-light active:dark:bg-accent-dark hover:bg-accent-light hover:dark:bg-accent-dark"
