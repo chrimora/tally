3dd47b2705fd312e2fb1a6cda4844130945c4612
chrimora <chris.d.moran@live.co.uk>
2023-10-18 18:09:35 +0100

    it works

--- src/app/play/game/page.tsx
+++ src/app/play/game/page.tsx
@@ -7,38 +7,32 @@ import { GameData, GameStorage, Score } from "../history";
 type PlayerProps = {
   index: number;
   update: (i: number, score: Score) => void;
-  load_score: Score;
+  score: Score;
 };
 
-function Player({ index, update, load_score }: PlayerProps) {
-  const [name, nameSetter] = useState("");
-  const [score, scoreSetter] = useState(0);
-
-  // Fix hydration errors
-  useEffect(() => {
-    nameSetter(load_score.name);
-    scoreSetter(load_score.score);
-  }, []);
-
-  useEffect(() => {
-    update(index, { name: name, score: score });
-  }, [score, name]);
+function Player({ index, update, score }: PlayerProps) {
+  function scoreSetter(n: number) {
+    update(index, { name: score.name, score: score.score + n });
+  }
+  function nameSetter(name: string) {
+    update(index, { name: name, score: score.score });
+  }
 
   // TODO; align + and - buttons https://stackoverflow.com/a/51526649
   return (
     <div key={`player${index}`} className="flex flex-col items-center">
       <div className="flex flex-row items-center">
-        <div className="text-9xl">{score}</div>
+        <div className="text-9xl">{score.score}</div>
         <div className="flex flex-col items-center">
           <button
             className="h-10 w-10 m-1 rounded-full text-xl bg-bgdim-light dark:bg-bgdim-dark active:border-2 hover:border-2 active:border-accent-light active:dark:border-accent-dark hover:border-accent-light hover:dark:border-accent-dark"
-            onClick={() => scoreSetter(score + 1)}
+            onClick={() => scoreSetter(+1)}
           >
             +
           </button>
           <button
             className="h-10 w-10 m-1 rounded-full text-xl bg-bgdim-light dark:bg-bgdim-dark active:border-2 hover:border-2 active:border-accent-light active:dark:border-accent-dark hover:border-accent-light hover:dark:border-accent-dark"
-            onClick={() => scoreSetter(score - 1)}
+            onClick={() => scoreSetter(-1)}
           >
             -
           </button>
@@ -47,91 +41,96 @@ function Player({ index, update, load_score }: PlayerProps) {
       <input
         type="text"
         onChange={(e) => nameSetter(e.target.value)}
-        value={name}
+        value={score.name}
         className="flex appearance-none outline-none bg-bgdim-light dark:bg-bgdim-dark py-2 rounded-xl text-center"
       />
     </div>
   );
 }
 
-class Game extends Component<{ reset: () => void }, GameData> {
-  constructor(props: any) {
-    super(props);
+type GameProps = {
+  state_reset: () => void;
+};
+
+function Game({ state_reset }: GameProps) {
+  function addPlayer() {
+    scoresSetter([
+      ...scores,
+      {
+        name: `player ${scores.length}`,
+        score: 0,
+      },
+    ]);
+  }
+
+  const [id, idSetter] = useState(nanoid());
+  const [scores, scoresSetter] = useState<Score[]>([]);
+
+  // Fix hydration errors
+  useEffect(() => {
+    // Init to two players
+    addPlayer();
 
     let load = GameStorage.read();
-    if (load) this.state = load;
-    else {
-      this.state = {
-        id: nanoid(),
-        scores: [],
-      };
-      this.state.scores.push(this.getNewScore());
-      this.state.scores.push(this.getNewScore());
+    if (load) {
+      idSetter(load.id);
+      scoresSetter(load.scores);
     }
+  }, []);
+
+  function store() {
+    GameStorage.store({ id: id, scores: scores });
+  }
+  function update(i: number, score: Score) {
+    let new_scores = [...scores];
+    new_scores[i] = score;
+
+    scoresSetter(new_scores);
+    store();
   }
 
-  getNewScore(): Score {
-    return {
-      name: `player ${this.state.scores.length}`,
-      score: 0,
-    };
+  function reset() {
+    GameStorage.wipe();
+    state_reset(); // TODO; check this state reset is still required - think localstorage is read every cycle
   }
-  addPlayer() {
-    let state: GameData = { ...this.state };
-    state.scores.push(this.getNewScore());
-    this.setState(state);
-    GameStorage.store(this.state);
+  function next() {
+    // TODO; add to history store and reset keeping players
   }
 
-  update(i: number, score: Score) {
-    let state = { ...this.state };
-    state.scores[i] = score;
-
-    this.setState(state);
-    GameStorage.store(this.state);
-  }
-
-  next() {
-    // TODO; add to history store
-    this.props.reset();
-  }
-
-  render() {
-    return (
-      <>
-        <div className="flex flex-wrap justify-center gap-2">
-          {this.state.scores.map((score, i) => (
-            <Player
-              index={i}
-              update={(i, s) => this.update(i, s)}
-              load_score={score}
-              key={i}
-            />
-          ))}
-          <button
-            className="h-15 w-15 m-1 rounded-full text-xl bg-bgdim-light dark:bg-bgdim-dark active:border-2 hover:border-2 active:border-accent-light active:dark:border-accent-dark hover:border-accent-light hover:dark:border-accent-dark"
-            onClick={() => this.addPlayer()}
-          >
-            +
-          </button>
-        </div>
-        <div>
-          <button
-            className="p-4 mx-2 mt-20 border-2 border-accent-light dark:border-accent-dark active:bg-accent-light active:dark:bg-accent-dark hover:bg-accent-light hover:dark:bg-accent-dark"
-            onClick={() => this.props.reset()}
-          >
-            Reset
-          </button>
-          <button
-            className="p-4 mx-2 mt-20 border-2 border-accent-light dark:border-accent-dark active:bg-accent-light active:dark:bg-accent-dark hover:bg-accent-light hover:dark:bg-accent-dark"
-            onClick={() => this.next()}
-          >
-            Next
-          </button>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className="flex flex-wrap justify-center gap-2">
+        {scores.map((score, i) => (
+          <Player
+            index={i}
+            update={(i, s) => update(i, s)}
+            score={score}
+            key={i}
+          />
+        ))}
+        <button
+          className="h-15 w-15 m-1 rounded-full text-xl bg-bgdim-light dark:bg-bgdim-dark active:border-2 hover:border-2 active:border-accent-light active:dark:border-accent-dark hover:border-accent-light hover:dark:border-accent-dark"
+          onClick={() => addPlayer()}
+        >
+          +
+        </button>
+      </div>
+      <div>
+        <button
+          className="p-4 mx-2 mt-20 border-2 border-accent-light dark:border-accent-dark active:bg-accent-light active:dark:bg-accent-dark hover:bg-accent-light hover:dark:bg-accent-dark"
+          onClick={() => reset()}
+        >
+          Reset
+        </button>
+        <button
+          className="p-4 mx-2 mt-20 border-2 border-accent-light dark:border-accent-dark active:bg-accent-light active:dark:bg-accent-dark hover:bg-accent-light hover:dark:bg-accent-dark"
+          onClick={() => next()}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default function Page() {
@@ -142,5 +141,5 @@ export default function Page() {
   }
 
   // Changing the key wipes the state - resetting it
-  return <Game key={key} reset={reset} />;
+  return <Game key={key} state_reset={reset} />;
 }
