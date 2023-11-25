4df2ce64f0d5d9d3634f07c10952c7ba6d375979
chrimora <chris.d.moran@live.co.uk>
2023-11-25 13:16:51 +0000

    message for no history

--- src/app/play/history/page.tsx
+++ src/app/play/history/page.tsx
@@ -30,33 +30,36 @@ function HistoryPage() {
   return (
     <>
       <div className="mx-auto text-sm sm:text-lg">
-        {history.groups.map((group, i) => (
-          <div
-            key={`${group.name}${i}`}
-            className="flex flex-row items-center mb-6"
-          >
-            <table>
-              <tbody>
-                {Object.entries(History.transformGroup(group)).map(
-                  ([name, amounts], k) => (
-                    <tr key={name} className={k != 0 ? "border-t" : ""}>
-                      <td className="w-40 truncate border-r pr-4 text-right">
-                        {name}
-                      </td>
-                      <td className="pr-4"></td>
-                      {amounts.map((amount, j) => (
-                        <td key={j} className="w-8 text-left">
-                          {amount}
+        {history.groups.length == 0 ? (
+          <p>No games yet. Go play!</p>
+        ) : (
+          history.groups.map((group, i) => (
+            <div
+              key={`${group.name}${i}`}
+              className="flex flex-row items-center mb-6"
+            >
+              <table>
+                <tbody>
+                  {Object.entries(History.transformGroup(group)).map(
+                    ([name, amounts], k) => (
+                      <tr key={name} className={k != 0 ? "border-t" : ""}>
+                        <td className="w-40 truncate border-r pr-4 text-right">
+                          {name}
                         </td>
-                      ))}
-                    </tr>
-                  ),
-                )}
-              </tbody>
-            </table>
-            <Link
-              href={`/play/game/load?id=${i}`}
-              className="
+                        <td className="pr-4"></td>
+                        {amounts.map((amount, j) => (
+                          <td key={j} className="w-8 text-left">
+                            {amount}
+                          </td>
+                        ))}
+                      </tr>
+                    ),
+                  )}
+                </tbody>
+              </table>
+              <Link
+                href={`/play/game/load?id=${i}`}
+                className="
               relative inline-block
               h-10 w-10 m-1 align-middle rounded-full active:border-2 hover:border-2
               active:border-accent-light dark:active:border-accent-dark hover:border-accent-light dark:hover:border-accent-dark
@@ -67,9 +70,10 @@ function HistoryPage() {
               before:w-1 before:my-2 before:mx-auto
               after:h-1 after:my-auto after:mx-2
               "
-            ></Link>
-          </div>
-        ))}
+              ></Link>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
